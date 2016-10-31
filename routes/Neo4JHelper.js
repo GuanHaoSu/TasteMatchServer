95f8b6fcff71810ff637ceb6f90db76f0521926c
GuanHaoSu <primebluer@gmail.com>
2016-10-31 17:29:12 +0800

    commit
    commit

---
+++ routes/Neo4JHelper.js
@@ -0,0 +1,400 @@
+﻿//neo4j driver operation reference from https://neo4j.com/developer/language-guides/
+var neo4j = require('neo4j-driver').v1;
+//var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "1234"));
+var connectString = "bolt://localhost:7687";
+var dbAccount = "neo4j";
+var dbPassword = "1234";
+
+exports.Login = function (res, UserName, token,id) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var query = "Merge(user:User{ name: {uName} }) " +
+               "Set user.token={token} " +
+               "Set user.id={id} " +
+               "return user";
+    session.run(query, { uName: UserName,token:token,id:id })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        res.send(result);
+      
+    })
+}
+function UpdatePreferGenreScores(res, UserId) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var query = "Match(user:User)-[r:likes]->(artist)-[r1:hasGenre]->(genre) " +
+                "Where user.id={UserId} "+
+                "with user as user, genre as genre, count(genre) as numOfGenre " +
+                "Merge(user)-[r2:prefer]->(genre) " +
+                "Set r2.score=numOfGenre "+
+                "return genre,r2.score "+
+                "order by r2.score desc ";
+    session.run(query, { UserId: UserId })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        res.send(result);
+      
+    })
+    
+};
+
+exports.CreatePeopleAndMusicNodes = function (res, result, UserName, UserId) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();  
+    var res = res;
+    var counter = 0;
+    var resultLenght = result.length;
+    var uName = UserName;
+
+    for (var i = 0; i < result.length; i++) {
+        
+        var bandName = result[i].bandName;
+        var NumberOfGenre = result[i].genre.length;
+        var query = "Merge (band:Artist{name:{bName}}) " +
+                    "Merge(people:User { name: {uName} }) " +
+                    "With band, people ";+
+                    "Where people.id={id} ";
+        var queryEnd = "Merge (people)-[:likes]->(band) ";
+     
+        for (var j = 0; j < NumberOfGenre; j++) {
+            var tempString = 
+            'Merge(genre'+j+':MusicGenre{ name: "'+result[i].genre[j].name +'"}) ' +
+            "Merge (band)-[:hasGenre]->(genre"+j+") ";
+            query= query.concat(tempString);
+        }
+        query = query.concat(queryEnd);
+        var tt = query;
+        //var query = "Merge (band:Artist{name:{bName}}) "+
+        //            "Merge(people:User { name: {uName} }) "+
+        //            "With band, people "+
+        //            "Merge(genre:MusicGenre{ name: {gName}}) "+
+        //            "Merge (band)-[r:hasGenre]->(genre) "+
+        //            "Merge (people)-[r1:likes]->(band) ";
+
+        session.run(query, {bName: bandName,uName:UserName,id: UserId})
+        .then(function (result) {
+            console.log(result);
+            if (counter === resultLenght - 1) {
+                UpdatePreferGenreScores(res, UserId);
+                session.close();
+                driver.close();
+            }
+            counter++;
+          
+        })
+    }
+
+};
+
+exports.PreferMusicGenreByPeople = function (res, UserID) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var query ="MATCH(u:User)-[r:prefer]->(n) " + 
+               "Where u.id={UserID} "+
+               "RETURN n.name as genreName, r.score "+
+               "order by r.score desc "+
+                "LIMIT 25"
+    session.run(query, { UserID: UserID })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        var JsonObj = new Object();
+        JsonObj["data"] = [];
+        for (var i = 0; i < result.records.length; i++) {
+            var genreName = result.records[i]._fields[0];
+            var Score = result.records[i]._fields[1].low;
+            var obj = new Object();
+            var comment;
+            if (Score < 50) {
+                Score = Score + 30;
+                
+            }
+            else if (Score > 100) {
+                Score = 100;
+                
+            }
+            else if (Score > 50&&Score<80) {
+                Score = Score + 20;
+                
+            }
+            if (Score <= 40) {
+                comment = "Somewhat enjoy";
+            }
+            else if (Score >= 70) {
+                comment = "Absolutely love";
+            }
+            else if (Score > 40 && Score < 70) {
+                comment = "Like it";
+            }
+
+            obj["genreName"] = genreName;
+            obj["Score"] = Score;
+            obj["comment"] = comment;
+            JsonObj.data.push(obj);
+        }
+        res.send(JsonObj);
+      
+    })
+
+}
+
+exports.RecommendArtists = function (res, UserID) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var query = "MATCH(u:User)-[r:prefer]->(n) " + 
+               "Where u.id={UserID} " +
+               "RETURN n.name as genreName " +
+               "order by r.score desc " +
+                "LIMIT 1"
+    session.run(query, { UserID: UserID })
+        .then(function (result) {
+        var genreName = result.records[0]._fields[0];
+        var subquery= "MATCH(a:MusicGenre { name:{genreName} })-[r:stylisticOrigins]->(b)<-[r2:stylisticOrigins]-(c:MusicGenre)<-[:hasGenre]-(artist:Artist), (u:User) "+
+                      "with count(b) as b , a as a, c as c, u, artist "+
+                      "Where u.id ={UserID} and not (u)-[:likes]->(artist) and not (u)-[:dislikes]->(artist) "+
+                      "return distinct artist.name limit 20"
+        return session.run(subquery, { UserID: UserID,genreName: genreName })
+
+    }).then(function (result) {
+        session.close();
+        driver.close();
+        var JsonObj = new Object();
+        JsonObj["data"] = [];
+        for (var i = 0; i < result.records.length; i++) {
+            var ArtistName = result.records[i]._fields[0];
+            JsonObj.data.push(ArtistName);
+        }
+        res.send(JsonObj);
+    })
+
+}
+
+exports.RecommendArtistsProfile = function (res, UserID, ArtistName) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var query = "MATCH(a:Artist { name: {ArtistName} })-[r1:hasGenre]->(g)<-[r2:hasGenre]-(b:Artist) "+
+                "with b "+
+                "Match(u:User) "+
+                "where u.id={UserID} and (u)-[:likes]-(b) "+
+                "return b.name, count(b) "+
+                "order by count(b) desc limit 5 "
+    session.run(query, { UserID: UserID, ArtistName: ArtistName })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        var JsonObj = new Object();
+        JsonObj["data"] = [];
+        for (var i = 0; i < result.records.length; i++) {
+            var artistName = result.records[i]._fields[0];
+            JsonObj.data.push(artistName);
+        }
+        res.send(JsonObj);
+      
+    })
+
+}
+
+exports.RecommendFriends = function (res, id) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var query = "Match (user:User)-[r:prefer]->(genre)<-[r1:prefer]-(user1:User) " +
+                "with sum(r.score*r1.score) as s, " +
+                "SQRT(reduce(a=0.0,n in collect(r.score)|a+n^2)) as aLength, " +
+                "SQRT(reduce(b=0.0,n in collect(r1.score)|b+n^2)) as bLength,user1,user " +
+                "with s/(aLength*bLength) as sim,user1,user " +
+                "where user.id={id} and not (user)-[:friend]-(user1) " +
+                "return  sim,user1.id,user1.name " +
+                "order by sim desc "
+    session.run(query, { id: id })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        var JsonObj = new Object();
+        JsonObj["data"] = [];
+        for (var i = 0; i < result.records.length; i++) {
+            var sim = result.records[i]._fields[0];
+            var id = result.records[i]._fields[1];
+            var name = result.records[i]._fields[2];
+            var comment = "";
+            var score = 0;
+            var dataObj = new Object();
+            if (sim > 0.95 && sim<=1) {
+                comment = "Super Match";
+                score = 100;
+            } else if (sim > 0.9 && sim <= 0.95) {
+                comment = "Good Match";
+                score = 80;
+            }
+            else if (sim > 0.8 && sim <= 0.9) {
+                comment = "Match";
+                score = 60;
+            }
+            else if (sim > 0.7 && sim <= 0.8) {
+                comment = "Little Match";
+                score = 40;
+            } else if (sim <= 0.7) {
+                comment = "Not too Match";
+                score = 20;
+            }
+
+            dataObj["sim"] = sim;
+            dataObj["id"] = id;
+            dataObj["name"] = name;
+            dataObj["comment"] = comment;
+            dataObj["score"] = score;
+            JsonObj.data.push(dataObj);
+        }
+        res.send(JsonObj);
+      
+    })
+
+}
+
+exports.RecommendFriendsProfile = function (res, UserID, RfriendID) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var query = "Match (user:User)-[r:prefer]->(genre)<-[r1:prefer]-(user1:User) " +
+                "where user.id={UserID} and user1.id ={RfriendID} " +
+                "return  genre.name " +
+                "order by r.score desc limit 10"
+    session.run(query, { UserID: UserID,RfriendID: RfriendID })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        var JsonObj = new Object();
+        JsonObj["data"] = [];
+        for (var i = 0; i < result.records.length; i++) {
+            var genreName = result.records[i]._fields[0];
+            JsonObj.data.push(genreName);
+        }
+        res.send(JsonObj);
+      
+    })
+
+}
+
+
+exports.RecommendFriendsLikeArtist = function (res, UserID, RfriendID) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var query = "MATCH(u)-[r:likes]->(artist)<-[r1:likes]-(u2) "+ 
+                "Where u.id ={UserID} and u2.id = {RfriendID} "+
+                "Return artist.name limit 10 "
+    session.run(query, { UserID: UserID, RfriendID: RfriendID })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        var JsonObj = new Object();
+        JsonObj["data"] = [];
+        for (var i = 0; i < result.records.length; i++) {
+            var artistName = result.records[i]._fields[0];
+            JsonObj.data.push(artistName);
+        }
+        res.send(JsonObj);
+      
+    })
+
+}
+
+
+exports.LikeArtist = function (res, UserID, ArtistName) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var UserID = UserID;
+    var query = "MATCH(u:User), (a:Artist { name:{ArtistName} }) " +
+                "Where u.id = {UserID} " +
+                "Merge(u)-[r:likes]->(a) " +
+                "RETURN u, r, a";
+    session.run(query, { UserID: UserID, ArtistName: ArtistName })
+        .then(function (result) {
+        console.log(result);
+        UserId = result.summary.statement.parameters.UserID;
+        UpdatePreferGenreScores(res, UserId);
+        session.close();
+        driver.close();
+      
+    })
+
+}
+
+exports.DislikeArtist = function (res, UserID, ArtistName) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var UserID = UserID;
+    var query = "MATCH(u:User)-[r:likes]->(a:Artist { name:{ArtistName} }) " +
+                "Where u.id = {UserID} " +
+                "Delete r " +
+                "RETURN u, r, a";
+    session.run(query, { UserID: UserID, ArtistName: ArtistName })
+        .then(function (result) {
+        console.log(result);
+        UserId=result.summary.statement.parameters.UserID;
+        UpdatePreferGenreScores(res, UserId);
+        session.close();
+        driver.close();
+      
+    })
+
+}
+
+exports.DontRecommendTheArtist = function (res, UserID, ArtistName) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var UserID = UserID;
+    var query = "MATCH(u:User),(a:Artist { name:{ArtistName} }) "+
+                "where u.id ={UserID} "+
+                "Merge(u)-[r:dislikes]-(a) "+
+                "return a, r, u"
+    session.run(query, { UserID: UserID, ArtistName: ArtistName })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        res.send(result)
+    })
+
+}
+
+
+exports.CheckUserLikesTheArtist = function (res, UserID, ArtistName) {
+    var driver = neo4j.driver(connectString, neo4j.auth.basic(dbAccount, dbPassword));
+    var session = driver.session();
+    var res = res;
+    var UserID = UserID;
+    var query = "MATCH(u)-[r:likes]->(a) "+
+                "where u.id = {UserID} and a.name = {ArtistName} "+
+                "RETURN u, a"
+    session.run(query, { UserID: UserID, ArtistName: ArtistName })
+        .then(function (result) {
+        console.log(result);
+        session.close();
+        driver.close();
+        if (result.records.length === 0) {
+            res.send(false);
+        }
+        else {
+            res.send(true);
+        }
+    })
+
+}
---
+++ routes/index.js
@@ -0,0 +1,151 @@
+﻿var express = require('express');
+var router = express.Router();
+var SparqlClient = require('sparql-client');
+var util = require('util');
+var Neo4j = require("./Neo4JHelper");
+var endpoint = 'http://dbpedia.org/sparql';
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: "MusicGraph" });
+});
+
+router.post('/Login', function (req, res) {
+    var JsonData = req.body;
+    var result = [];
+    var UserName = JsonData.name;
+    var token = JsonData.token;
+    var id = JsonData.id;
+    if (token != "" && id!="") {
+        Neo4j.Login(res, UserName, token,id);
+    }
+    else {
+        res.send('token is not valid');
+    }
+  
+});
+router.post('/', function (req, res) {
+    var JsonData = req.body;
+    var counter = 0;
+    var result = [];
+    var UserName = JsonData.name;
+    var token = JsonData.token;
+    var UserId= JsonData.id;
+    if (token != "") {
+        for (var i = 0; i < JsonData.data.length; i++) {
+            var name = JsonData.data[i].name;
+            var ArtistName = '"' + name + '"';
+            var GetGenre = "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+                   "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
+                   "prefix dbp:  <http://dbpedia.org/property/> " + 
+                   "SELECT ?bandName ?GenreName  WHERE { " +
+                   "?band dbp:name ?bandName. " +
+                   " ?band <http://dbpedia.org/ontology/genre> ?genre. " +
+                   "?genre rdfs:label ?GenreName " + 
+                    "FILTER (lang(?GenreName ) = 'en') " +
+                    "FILTER (str(?bandName) =" + ArtistName + ") " +
+                    "FILTER (lang(?bandName) = 'en')} ";
+            var client = new SparqlClient(endpoint);
+            console.log("Query to " + endpoint);
+            console.log("Query: " + GetGenre);
+            client.query(GetGenre)
+            .execute(function (error, results) {
+                
+                
+                if (results!=null&& results.results.bindings.length>0) {
+                    var resultLength = results.results.bindings.length;
+                    var resultObj = new Object();
+                    resultObj["genre"] = [];
+                    var bandName = results.results.bindings[0].bandName.value;
+                    resultObj["bandName"] = bandName;
+                    for (var i = 0; i < resultLength; i++) {
+                        var genreName = results.results.bindings[i].GenreName.value;
+                        resultObj["genre"].push({ "name": genreName });
+                
+                    }
+                    result.push(resultObj);
+                }
+                
+                if (counter === JsonData.data.length - 1) {
+                    Neo4j.CreatePeopleAndMusicNodes(res, result, UserName, UserId);
+               // res.send(result);
+                }
+                counter++;
+                process.stdout.write(util.inspect(arguments, null, 20, true) + "\n"); 1
+            });
+        }
+    }
+    else { 
+      res.send('token is not valid');
+    }
+  
+});
+router.get('/PreferMusicGenre/:id', function (req, res) {
+    var UserID = req.params.id;
+    Neo4j.PreferMusicGenreByPeople(res, UserID);
+});
+
+router.get('/RecommendArtists/:id', function (req, res) {
+    var id = req.params.id;
+    Neo4j.RecommendArtists(res, id);
+});
+
+router.post('/RecommendArtistsProfile', function (req, res) {
+    var JsonData = req.body;
+    var ArtistName = JsonData.ArtistName;
+    var UserId = JsonData.id;
+    Neo4j.RecommendArtistsProfile(res, UserId, ArtistName);
+});
+
+
+router.get('/RecommendFriends/:id', function (req, res) {
+    var id = req.params.id;
+    Neo4j.RecommendFriends(res, id);
+});
+
+router.get('/RecommendFriendsProfile/:id/:RfriendsID', function (req, res) {
+    var UserId = req.params.id;
+    var RfriendsID = req.params.RfriendsID;
+    Neo4j.RecommendFriendsProfile(res, UserId, RfriendsID);
+});
+
+router.get('/RecommendFriendsLikeArtist/:id/:RfriendsID', function (req, res) {
+    var UserId = req.params.id;
+    var RfriendsID = req.params.RfriendsID;
+    Neo4j.RecommendFriendsLikeArtist(res, UserId, RfriendsID);
+});
+
+router.post('/LikeArtist', function (req, res) {
+    var JsonData = req.body;
+    var ArtistName = JsonData.ArtistName;
+    var UserId = JsonData.id;
+
+    Neo4j.LikeArtist(res, UserId, ArtistName);
+});
+
+router.post('/DislikeArtist', function (req, res) {
+    var JsonData = req.body;
+    var ArtistName = JsonData.ArtistName;
+    var UserId = JsonData.id;
+    Neo4j.DislikeArtist(res, UserId, ArtistName);
+});
+
+router.post('/DontRecommendTheArtist', function (req, res) {
+    var JsonData = req.body;
+    var ArtistName = JsonData.ArtistName;
+    var UserId = JsonData.id;
+    Neo4j.DontRecommendTheArtist(res, UserId, ArtistName);
+});
+
+router.post('/CheckUserLikesTheArtist', function (req, res) {
+    var JsonData = req.body;
+    var ArtistName = JsonData.ArtistName;
+    var UserId = JsonData.id;
+    Neo4j.CheckUserLikesTheArtist(res, UserId, ArtistName);
+});
+
+router.post('/test', function (req, res) {
+    var result = req.body;
+    res.send(result);
+});
+module.exports = router;
